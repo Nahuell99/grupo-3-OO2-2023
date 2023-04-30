8894210bd8abfe33eac0d4405ea41fec9e7d84df
Nahuell99 <Naahuelll99@gmail.com>
2023-04-30 00:00:49 -0300

    Initial commit

---
+++ src/main/java/com/unla/SpringBootUnLa/SpringBootUnLaApplication.java
@@ -0,0 +1,13 @@
+package com.unla.SpringBootUnLa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnLaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUnLaApplication.class, args);
+	}
+
+}
