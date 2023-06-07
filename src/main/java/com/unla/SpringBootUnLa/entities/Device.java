4778a98d01605d501ad0ec0846020910d36c19d2
igmansilla <igmansilla98@gmail.com>
2023-06-07 15:33:15 -0300

    changes en divice

--- src/main/java/com/unla/SpringBootUnLa/entities/Device.java
+++ src/main/java/com/unla/SpringBootUnLa/entities/Device.java
@@ -17,11 +17,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-@Entity @Getter @Setter
+@Entity @Getter @Setter @EqualsAndHashCode @ToString
 @NoArgsConstructor
 public abstract class Device {
 
@@ -47,34 +50,12 @@ public abstract class Device {
 	@OneToMany(mappedBy = "device", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Event> eventos = new ArrayList<>();
 
-	public Device(String nombre, String descripcion, boolean activo) {
+	protected Device(String nombre, String descripcion, boolean activo) {
 		super();
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 		this.activo = activo;
 	}
 
-	@Override
-	public String toString() {
-		return "Device [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", activo=" + activo
-				+ ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Device other = (Device) obj;
-		return id == other.id;
-	}
 
 }
