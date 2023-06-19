61fdddba89471dc68ea106def050f276dfa165f0
igmansilla <igmansilla98@gmail.com>
2023-06-19 18:47:35 -0300

    change indentado

--- src/main/java/com/unla/SpringBootUnLa/helpers/ViewRouteHelper.java
+++ src/main/java/com/unla/SpringBootUnLa/helpers/ViewRouteHelper.java
@@ -2,21 +2,23 @@ package com.unla.SpringBootUnLa.helpers;
 
 public class ViewRouteHelper {
 
-	private ViewRouteHelper(){}
-    public static final  String MENU_OPCIONES 							= "device/sensorAlumbradoInteligente";
-	public static final  String CREAR_ALUMBRADO_INTELIGENTE 				= "device/sensorAlumbradoInteligente/crearSensor";
-	public static final  String NUEVO_ALUMBRADO_INTELIGENTE 				= "device/sensorAlumbradoInteligente/nuevoSensor";
-	public static final  String ELIMINAR_ALUMBRADO_INTELIGENTE 			= "device/sensorAlumbradoInteligente/eliminarSensor";
-	public static final  String EDITAR_ALUMBRADO_INTELIGENTE 			= "device/sensorAlumbradoInteligente/editarSensor";
-	public static final  String SIN_DEVICE								= "device/sensorAlumbradoInteligente/sinDevice";
-	public static final  String FORMULARIO_EDITAR_ALUMBRADO_INTELIGENTE 	= "device/sensorAlumbradoInteligente/editarSensorFormulario";
-	public static final  String LISTA_ALUMBRADO_INTELIGENTE 				= "device/sensorAlumbradoInteligente/listaSensores";
-	
-	public static final String MEDICIONES_ALUMBRADO_INTELIGENTE 		= "device/sensorAlumbradoInteligente/mediciones/mediciones";
-	public static final String EVENTO_ALUMBRADO_INTELIGENTE 			= "device/sensorAlumbradoInteligente/mediciones/eventos";
-	public static final String SIN_MEDICIONES_NUEVAS 					= "device/sensorAlumbradoInteligente/mediciones/sinMedicionesNuevas";
-	public static final String SIN_MEDICIONES							= "device/sensorAlumbradoInteligente/mediciones/sinMediciones";
-	public static final String SIN_EVENTOS								= "device/sensorAlumbradoInteligente/mediciones/sinEventos";
+    private ViewRouteHelper() {
+    }
+
+    public static final String MENU_OPCIONES = "device/sensorAlumbradoInteligente";
+    public static final String CREAR_ALUMBRADO_INTELIGENTE = "device/sensorAlumbradoInteligente/crearSensor";
+    public static final String NUEVO_ALUMBRADO_INTELIGENTE = "device/sensorAlumbradoInteligente/nuevoSensor";
+    public static final String ELIMINAR_ALUMBRADO_INTELIGENTE = "device/sensorAlumbradoInteligente/eliminarSensor";
+    public static final String EDITAR_ALUMBRADO_INTELIGENTE = "device/sensorAlumbradoInteligente/editarSensor";
+    public static final String SIN_DEVICE = "device/sensorAlumbradoInteligente/sinDevice";
+    public static final String FORMULARIO_EDITAR_ALUMBRADO_INTELIGENTE = "device/sensorAlumbradoInteligente/editarSensorFormulario";
+    public static final String LISTA_ALUMBRADO_INTELIGENTE = "device/sensorAlumbradoInteligente/listaSensores";
+
+    public static final String MEDICIONES_ALUMBRADO_INTELIGENTE = "device/sensorAlumbradoInteligente/mediciones/mediciones";
+    public static final String EVENTO_ALUMBRADO_INTELIGENTE = "device/sensorAlumbradoInteligente/mediciones/eventos";
+    public static final String SIN_MEDICIONES_NUEVAS = "device/sensorAlumbradoInteligente/mediciones/sinMedicionesNuevas";
+    public static final String SIN_MEDICIONES = "device/sensorAlumbradoInteligente/mediciones/sinMediciones";
+    public static final String SIN_EVENTOS = "device/sensorAlumbradoInteligente/mediciones/sinEventos";
 
     // Vistas Recolector inteligente
     public static final String MENU_OPCIONES_RECOLECTOR = "device/recolectorInteligente";
