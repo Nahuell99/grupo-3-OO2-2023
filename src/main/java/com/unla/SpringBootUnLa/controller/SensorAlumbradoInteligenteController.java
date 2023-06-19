96a76dfff251ed932b79a5d7404701a1d7964a45
igmansilla <igmansilla98@gmail.com>
2023-06-19 17:25:55 -0300

    add exception

--- src/main/java/com/unla/SpringBootUnLa/controller/SensorAlumbradoInteligenteController.java
+++ src/main/java/com/unla/SpringBootUnLa/controller/SensorAlumbradoInteligenteController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/unla/SpringBootUnLa/entities/Device.java
+++ src/main/java/com/unla/SpringBootUnLa/entities/Device.java
@@ -3,7 +3,6 @@ package com.unla.SpringBootUnLa.entities;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
